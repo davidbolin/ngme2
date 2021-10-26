9c82203dc6cdb5f8a7f670d1b07f304de2f0be83
Xiaotian Jin <jinxiaotian_sal@outlook.com>
2021-10-26 22:30:08 +0300

    Adding model functions

---
+++ src/model.cpp
@@ -0,0 +1,78 @@
+#include "model.h"
+
+// Property of K
+VectorXd Model::grad_mu_1(const VectorXd& w,
+                          const VectorXd& V,
+                          const VectorXd& Y) 
+{
+    // VectorXd m = K.selfadjointView<Eigen::Upper>().llt().solve(-mu * h + mu * V);
+    
+    VectorXd m = K.partialPivLu().solve(-mu * h + mu * V);
+    VectorXd r = pow(sigma_eps, -2) * A * K.partialPivLu().solve(-h + V) * 
+                   V.cwiseInverse().asDiagonal() * 
+                   (Y - A * K.partialPivLu().solve(-mu * h + mu * V) - A * (w - m));
+    return r;
+}
+
+double Model::grad_sgima_eps_1(const VectorXd& w,
+                             const VectorXd& V,
+                             const VectorXd& Y) 
+{
+    VectorXd m = K.partialPivLu().solve(-mu * h + mu * V);
+    VectorXd x = Y - A * K.partialPivLu().solve(-mu * h + mu * V) - A * (w - m);
+    double r = -n / sigma_eps + 1 / pow(sigma_eps, 3) * 
+                 x.transpose() * V.cwiseInverse().asDiagonal() * x;
+
+    return r;
+}
+
+// double Model::grad_kappa_sq_1(const VectorXd& w,
+//                             const VectorXd& V,
+//                             const VectorXd& Y) 
+// {
+//     double r = (C * K.inverse()).trace() - w.transpose() * C.transpose() * 
+//                 V.cwiseInverse().asDiagonal() * 
+//                 (K*w + (h-V) * mu);
+//     return r;
+// }
+
+// grad of mu for all samples V and Y
+// d * num_samples
+VectorXd Model::grad_mu(const MatrixXd& ws,
+                        const MatrixXd& Vs,
+                        const VectorXd& Y) 
+{
+    int samples = ws.cols();
+    MatrixXd grads (A.rows(), samples);
+
+    // compute grad for every sample
+    for (int i = 1; i < samples; i++) {
+        grads.col(i) = grad_mu_1(ws.col(i), Vs.col(i), Y);
+    }
+
+    return grads.rowwise().mean();
+}
+
+double Model::grad_sgima_eps(const MatrixXd& ws,
+                     const MatrixXd& Vs,
+                     const VectorXd& Y)
+{
+    int samples = ws.cols();
+    VectorXd grads (samples);
+    for (int i = 1; i < samples; i++) {
+        grads(i) = grad_sgima_eps_1(ws.col(i), Vs.col(i), Y);
+    }
+    return grads.mean();
+}
+
+double Model::grad_kappa_sq(const MatrixXd& ws,
+                     const MatrixXd& Vs,
+                     const VectorXd& Y)
+{
+    int samples = ws.cols();
+    VectorXd grads (samples);
+    for (int i = 1; i < samples; i++) {
+        grads(i) = grad_kappa_sq_1(ws.col(i), Vs.col(i), Y);
+    }
+    return grads.mean();
+}
--- src/test-opt.cpp
+++ src/test-opt.cpp
@@ -9,14 +9,29 @@
 
 context("tests for optimizer") {
 
-    test_that("simple test for gd") {
+    test_that("convergence for gd") {
         model f;
         optimizer opt;
-        Eigen::Vector2d x0 (5,5);
-        VectorXd out = opt.gd(f, x0, 0.05, 0.01, false);
+        Eigen::Vector2d x0 (10, 10);
 
-        // close to 0
-        expect_true(out.norm() < 1);
+        VectorXd out = opt.sgd(f, x0, 0.05, 0.01, false);
+
+        // optimum should be (-1/6, -3/4)
+        expect_true(out(0) + 1.0/6 < 0.001);
+        expect_true(out(1) + 3.0/4 < 0.001);
+    }
+
+
+    // Use Hessian as preconditoner
+    test_that("convergence using hessian") {
+        model f;
+        optimizer opt;
+        Eigen::Vector2d x0(10, 10);
+
+        VectorXd out = opt.sgd(f, x0, 0.05, 0.01, true);
+
+        expect_true(out(0) + 1.0/6 < 0.001);
+        expect_true(out(1) + 3.0/4 < 0.001);
     }
 
 }

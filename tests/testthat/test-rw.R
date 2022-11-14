f19b4c42fd14717c9d40ba6c8daba6baff1a8aa9
vpnsctl <alexandre.impa@gmail.com>
2022-11-15 00:38:03 +0300

    Adding even more dependencies

--- tests/testthat/test-rw.R
+++ tests/testthat/test-rw.R
@@ -17,23 +17,24 @@ test_that("the order of W same as order of index?", {
 # [1,] .         .         0.7142857 0.2857143
 # [2,] 0.4545455 0.5454545 .         .
 
-  # the order is wrong, recover by permutation matrix
-  A %*% as(mesh1$idx$loc, "pMatrix")
+## Deprecated
+#   # the order is wrong, recover by permutation matrix
+#   A %*% as(mesh1$idx$loc, "pMatrix")
 
-  rw1$C + rw1$G
-# 3 x 4 sparse Matrix of class "dgCMatrix"
-# [1,] 1 -1  .  .
-# [2,] .  1 -1  .
-# [3,] .  .  1 -1
+#   rw1$C + rw1$G
+# # 3 x 4 sparse Matrix of class "dgCMatrix"
+# # [1,] 1 -1  .  .
+# # [2,] .  1 -1  .
+# # [3,] .  .  1 -1
 
-  A %*% mesh1$loc
+#   A %*% mesh1$loc
 
-  sorted_index <- sort(XX, index.return = TRUE)
-  A2 <- Matrix::sparseMatrix(seq_along(XX), sorted_index$ix, x = 1); A2
+#   sorted_index <- sort(XX, index.return = TRUE)
+#   A2 <- Matrix::sparseMatrix(seq_along(XX), sorted_index$ix, x = 1); A2
 
-  # rw2
-  rw2 <- model_rw(1:4, order = 2, circular = F)
-  rw2$C + rw2$G
+#   # rw2
+#   rw2 <- model_rw(1:4, order = 2, circular = F)
+#   rw2$C + rw2$G
 
-  sparseMatrix(1:10, 1:10, x = 1)
+#   sparseMatrix(1:10, 1:10, x = 1)
 })

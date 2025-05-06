add0ff47d16ad49a95c27c990135efd7920c6388
Xiaotian Jin <jin.xiaotian.kim@gmail.com>
2025-05-06 14:38:34 +0300

    upadtes

--- src/latents/generic.cpp
+++ src/latents/generic.cpp
@@ -19,39 +19,6 @@ void General::update_dK(const VectorXd& theta_K) {
     // update dK using theta_K
 }
 
-// VectorXd General::param_trans_fun(const VectorXd& theta_K, const std::string& trans) const {
-//     //  -1 + (2 * exp(th)) / (1 + exp(th))
-//     auto th2a = [](double th) {
-//         return -1 + (2 * exp(th)) / (1 + exp(th));
-//     };
-
-//     // transform theta_K using trans_name
-//     if (trans == "matern") {
-//         Vector2d trans_theta = Vector2d::Zero();    
-//         trans_theta(0) = exp(2 * theta_K(0));
-//         trans_theta(1) = 1;
-//         return trans_theta;
-//     } else if (trans_type == "ar1 x matern") {
-//         Vector4d trans_theta = Vector4d::Zero();
-//         trans_theta(0) = th2a(theta_K(0)) * exp(2 * theta_K(1));
-//         trans_theta(1) = th2a(theta_K(0));
-//         trans_theta(2) = exp(2 * theta_K(1));
-//         trans_theta(3) = 1;
-//         return trans_theta;
-//     } else {
-//         return theta_K;
-//     }
-// }
-
-
-// template of idx_mat
-//        rho  beta kappa
-// [1,]  TRUE FALSE  TRUE
-// [2,] FALSE  TRUE  TRUE
-// [3,]  TRUE FALSE FALSE
-// [4,] FALSE  TRUE FALSE
-
-
 VectorXd General::compute_coef(const VectorXd& theta_K, const MatrixXd& idx_mat, const std::vector<std::string>& trans) const {
     int nrow = idx_mat.rows();
     VectorXd coef = VectorXd::Ones(nrow);
